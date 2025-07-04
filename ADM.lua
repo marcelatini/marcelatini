51609fdf3552b69cea030c445eb74025f45f7881
marcelatini <skunkstore-marcelatini@yahoo.com.br>
2025-07-03 21:30:40 -0300

    ADM.lua

--- ADM.lua
+++ ADM.lua
@@ -19,7 +19,8 @@
 |carolinaygh|223|
 |ohmakbarak|683|
 |medsonelemental|245|
-|moranguinhomalandro|241|
 |prittokarumak|243|
 |arluttano|297|
 |marcelatini|295|
+|moranguinhomalandro|241|
+|lunaabara|331|
