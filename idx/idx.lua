50a8d9d6e000642bcc3c811fcfbf36c1fcda8d2d
marcelatini <skunkstore-marcelatini@yahoo.com.br>
2025-08-14 08:23:03 -0300

    Add files via upload

--- idx/idx.lua
+++ idx/idx.lua
@@ -1,3 +1,3 @@
-|data|path|Fluff1.lua, 12, 18|2, $min, $max|
-|data|path|Fluff2.lua, 12, 13|2, 30, 31|4, 40, 41|
-|data|path|Fluff3.lua, 12, 13|2, 30, 31|4, 40, 41|6, 60, 61|
+|data|Fluff1.lua|3, 12, 18|2, $min, $max|
+|data|Fluff2.lua|3, 12, 13|2, 30, 31|4, 40, 41|
+|data|Fluff3.lua|3, 12, 13|2, 30, 31|4, 40, 41|6, 60, 61|
